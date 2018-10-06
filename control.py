6d25e77f1effd50393df2a3fa422652d34bec7c9
DrunkOnHotCoco <31522768+DrunkOnHotCoco@users.noreply.github.com>
2018-10-06 01:12:02 +0000

    Add files via upload

--- control.py
+++ control.py
@@ -4,20 +4,24 @@ import time #These are the required libraries (make sure they are installed)
 
 def sendPacket(byte): #user defines packet
     packet = [byte] #now we will send this packet over to the arduino
-
+    
     for i in packet: #probably a better way to do this
         elements = [i]
         print( bytes(elements))
-        ser.write(bytes(elements)) #sends packets over xbee
-    time.sleep(.005) #wait a bit between sending
-
+        print(ser.write(bytes(elements))) #sends packets over xbee
+    #time.sleep(.005) #wait a bit between sending
+    
     return
+#argument should be an int corresponding to current button state (1/0)
+def sendButtonPacket(button_state):
+    ser.write(button_state)
 
+    
 ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1) #this is the port for your xbee
 
 
 pygame.joystick.init()
-print("Joysticks found: %s" % str(pygame.joystick.get_count()))
+#print("Joysticks found: %s" % str(pygame.joystick.get_count()))
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())] #pygame will find any number of joysticks present on your computer
 
 
@@ -33,7 +37,8 @@ for i in range(12):#create an array to put button values in
 
 
 while done==False:
-    print("Joysticks found: %s" % str(pygame.joystick.get_count()))
+    print("=====================")
+    #print("Joysticks found: %s" % str(pygame.joystick.get_count()))
     for event in pygame.event.get(): #whenever we quit pygame pygame will end
         if event.type == pygame.QUIT:
             done=True
@@ -44,19 +49,23 @@ while done==False:
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
         buttonPacket = 0
-        for j in range(5): #send the first few values of buttons (adding more and decreasing the delay on sending packets seems to make recieved packets less accurate)
-            buttonPacket = (buttonPacket<<1)+joystick.get_button(j)
+        """for j in range(5): #send the first few values of buttons (adding more and decreasing the delay on sending packets seems to make recieved packets less accurate)
+            buttonPacket = (buttonPacket)+joystick.get_button(j)
+        """
+        buttonPacket = int(joystick.get_button(0))
+        #print("Found Button Press, NOT sending! " + str(buttonPacket))
+        button_1 = joystick.get_button(0)
+        print("Button 1: %s " % str(button_1))
+        print("Button 2: %s " % str(joystick.get_button(1)))
+        print("Button 3: %s " % str(joystick.get_button(2)))
+        print("Button 4: %s " % str(joystick.get_button(3)))
+        #print("Read: %s" % str(ser.read(3)))
+        print("Read: %s" % str(ser.readline()))
 
-
-        #sendPacket(((2)<<5)+buttonPacket)
-        print("Found Button Press, NOT sending! " + str(buttonPacket))
-
-
-
-
+        
         Xaxis = round(joystick.get_axis(0) * 15 + 15) #this should be the "drone" setup for X and Y axes.  The last joystick found is the primary
         Yaxis = round(joystick.get_axis(3)* 15 + 15)
-
+        print("Type X Axis: %s" % str(type(Xaxis)))
         #The joystick axis are indexed at 0, so Left X = 0, Left Y = 1, Right X = 2, Right Y = 3 and so on if you have more
 
         print("X axis = " + str(Xaxis) + " Y axis = " + str(Yaxis))
@@ -64,5 +73,6 @@ while done==False:
         packet = [Xaxis, Yaxis + ( 1 << 5)] #now we will send this packet over to the arduino
         sendPacket(Xaxis)
         sendPacket(Yaxis)
+        sendPacket(button_1)
         #sendPacket(Yaxis + (1 << 5))
         time.sleep(.05)     #delay is needed for accuracy on arduino
